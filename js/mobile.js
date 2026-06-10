1207afe9a197d0f1cf3f41499411178916e556f3
条野太郎 <33174970+ckfeel@users.noreply.github.com>
2026-06-10 13:52:57 +0800

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -15,7 +15,7 @@
         if (!dom.mobileToolbarTitle) {
             return;
         }
-        dom.mobileToolbarTitle.textContent = "Solara";
+        dom.mobileToolbarTitle.textContent = "⑤号音乐库";
     }
 
     function updateMobileOverlayScrim() {
